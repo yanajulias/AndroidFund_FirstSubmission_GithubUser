9ba07bf2ddda69a74caf8c964208afc4874fc753
Yana <44026935+yanajulias@users.noreply.github.com>
2023-05-26 04:38:10 +0700

    Initial commit

---
+++ app/src/main/java/dicoding/first/submission/githubuser/MainActivity.kt
@@ -0,0 +1,11 @@
+package dicoding.first.submission.githubuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
