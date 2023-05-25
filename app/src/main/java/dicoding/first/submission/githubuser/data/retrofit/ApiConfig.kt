113272fd7208226893cf9d147f471f0fd894f697
Yana <44026935+yanajulias@users.noreply.github.com>
2023-05-26 06:49:38 +0700

    Add ApiService.kt and the Response for each endpoint

--- app/src/main/java/dicoding/first/submission/githubuser/data/retrofit/ApiConfig.kt
+++ app/src/main/java/dicoding/first/submission/githubuser/data/retrofit/ApiConfig.kt
@@ -1,5 +1,6 @@
 package dicoding.first.submission.githubuser.data.retrofit
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,10 +9,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
-            val loggingInterceptor =
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+//            val loggingInterceptor =
+//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val authInterceptor = Interceptor { chain ->
+                val req = chain.request()
+                val requestHeaders = req.newBuilder()
+                    .addHeader("Authorization", "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")
+                    .build()
+                chain.proceed(requestHeaders)
+            }
             val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
+                .addInterceptor(authInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
--- app/src/main/java/dicoding/first/submission/githubuser/data/retrofit/ApiService.kt
+++ app/src/main/java/dicoding/first/submission/githubuser/data/retrofit/ApiService.kt
@@ -1,8 +1,29 @@
 package dicoding.first.submission.githubuser.data.retrofit
 
+import dicoding.first.submission.githubuser.data.response.DetailUserResponse
+import dicoding.first.submission.githubuser.data.response.FollowersResponse
+import dicoding.first.submission.githubuser.data.response.FollowingResponse
+import dicoding.first.submission.githubuser.data.response.SearchResponse
+import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
 
-class ApiService {
+interface ApiService {
 
+    @GET("search/users")
+    fun getListUser(@Query("q") q: String): Call<SearchResponse>
+
+    @GET("users/{username}")
+    fun getDetailUser(@Path("username") username: String): Call<DetailUserResponse>
+
+    @GET("users/{username}/followers")
+    fun getFollowersUser(
+        @Path("username") username: String
+    ): Call<List<FollowersResponse>>
+
+    @GET("users/{username}/following")
+    fun getFollowingUser(
+        @Path("username") username: String
+    ): Call<List<FollowingResponse>>
 }
