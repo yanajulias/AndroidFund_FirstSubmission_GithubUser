4353a363d4924de610460652734e5ad944e67c7b
Yana <44026935+yanajulias@users.noreply.github.com>
2023-05-28 20:00:08 +0700

    Add onClickListener to each username to DetailUserActivity.kt

--- app/src/main/java/dicoding/first/submission/githubuser/adapter/UserListAdapter.kt
+++ app/src/main/java/dicoding/first/submission/githubuser/adapter/UserListAdapter.kt
@@ -6,10 +6,10 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.transform.CircleCropTransformation
 import dicoding.first.submission.githubuser.data.response.ItemsItem
-import dicoding.first.submission.githubuser.data.response.ListResponse
 import dicoding.first.submission.githubuser.databinding.ItemUserBinding
 
-class UserListAdapter(private val data: MutableList<ItemsItem> = mutableListOf()) :
+class UserListAdapter(private val data: MutableList<ItemsItem> = mutableListOf(),
+private val listener: (ItemsItem) -> Unit) :
     RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
 
     fun setData(data: MutableList<ItemsItem>) {
@@ -37,6 +37,9 @@ class UserListAdapter(private val data: MutableList<ItemsItem> = mutableListOf()
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val item = data[position]
         holder.bind(item)
+        holder.itemView.setOnClickListener {
+            listener(item)
+        }
     }
 
 }
