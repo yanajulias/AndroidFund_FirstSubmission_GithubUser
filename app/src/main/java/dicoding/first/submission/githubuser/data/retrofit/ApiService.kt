def9bc3143c6f4fac9dd4541ac73197ce8e5dd3a
Yana <44026935+yanajulias@users.noreply.github.com>
2023-05-29 08:40:17 +0700

    Change endpoint

--- app/src/main/java/dicoding/first/submission/githubuser/data/retrofit/ApiService.kt
+++ app/src/main/java/dicoding/first/submission/githubuser/data/retrofit/ApiService.kt
@@ -2,17 +2,16 @@ package dicoding.first.submission.githubuser.data.retrofit
 
 import dicoding.first.submission.githubuser.data.response.DetailUserResponse
 import dicoding.first.submission.githubuser.data.response.ItemsItem
+import dicoding.first.submission.githubuser.data.response.ListResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
 
-//    @GET("search/users")
-//    fun getListUser(@QueryMap() map: Map<String, Any>): Call<ListResponse>
-
-    @GET("users")
-    fun getListUser(): Call<MutableList<ItemsItem>>
+    @GET("search/users")
+    fun getListUser(@Query("q") query: String): Call<ListResponse>
 
     @GET("users/{username}")
     fun getDetailUser(@Path("username") username: String): Call<DetailUserResponse>
